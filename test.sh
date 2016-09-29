f3478900bd6db57bb5def7227d5290b1f48f7c34
Eric Bridgeford <ebridge2@jhu.edu>
2016-09-29 18:45:49 -0400

    should fix issue with OSX

--- test.sh
+++ test.sh
@@ -27,22 +27,28 @@ echo "option | algorithm | accuracy | duration"
 # loop over everything...
 for opt in "${options[@]}"; do
     for algo in "${algorithm[@]}"; do
-        start=$(($(date +%s%N)/1000000)) # get the time
+        # fun little python trick
+        start=$(python -c'import time; print(str(time.time()))') # get the time
+
         # capture stderr in a vara
         trainout=$(python ${1}classify.py --mode train --algorithm $algo --model-file datasets/${opt}.perceptron.model --data datasets/${opt}.train 2>&1)
 
         # capture stderr in a vara
         testout=$(python ${1}classify.py --mode test --model-file datasets/${opt}.perceptron.model --data datasets/${opt}.dev --predictions-file datasets/${opt}.dev.predictions 2>&1)
-        end=$(($(date +%s%N)/1000000))
-        duration=$(( end - start ))
+        duration=$(python -c'import time; print(str(time.time() - float('$start')))')
 
         # make sure we didn't get any errors
         if [[ $trainout == *"Error"* ]] || [[ $testout == *"Error"* ]]; then
             echo "Your code doesn't work for this algorithm :("
+            if [[ $trainout == *"Error"* ]]; then
+                echo "$trainout"
+            else
+                echo "$testout"
+            fi
         else
             acc="$(python compute_accuracy.py datasets/${opt}.dev datasets/${opt}.dev.predictions)"
 
-            echo "${opt} | $algo | $acc | $duration (ms)"
+            echo "${opt} | $algo | $acc | $duration (s)"
         fi
     done
 done
